30c7b2752d80e662d443786164ae407fc8ef9ae4
Arthur <arthurquique@protonmail.com>
2021-01-08 13:49:58 +0100

    Initial commit

---
+++ src/tnsi/tp/Test.java
@@ -0,0 +1,19 @@
+package tnsi.tp;
+
+public class Test {
+
+    /**
+     * Générateur de CSP
+     * @param nbV nombre de variables
+     * @param taille taille des domaines
+     * @param densite taux d'arcs
+     * @param durete taux de contraintes à respecter
+     */
+    public void randomCspGenerator(int nbV, int taille, int densite, int durete) {
+
+    }
+
+    public static void main (String[] args) {
+
+    }
+}
