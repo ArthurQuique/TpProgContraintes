57bb0f5c8c2cac0498b2e19f2441b2135563e5d9
Arthur <arthurquique@protonmail.com>
2021-01-09 14:40:18 +0100

    liste domaine

--- src/tnsi/tp/Summit.java
+++ src/tnsi/tp/Summit.java
@@ -1,16 +1,28 @@
 package tnsi.tp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Summit {
     private int id;
     private int value;       //Valeur de l'objet
     private int min;         //Borne inférieure du domaine de définition
     private int max;         //Borne supérieure du domaine de définition
+    private List<Integer> domain; // Domaine de définition
 
     public Summit(int id, int taille) {
         this.id = id;
         this.min = 1;
         this.max = taille;
         this.value = 0;
+        this.domain = new ArrayList<>();
+        for (int i = this.min; i <= this.max; i++) {
+            this.domain.add(i);
+        }
+    }
+
+    public void affectToValue() {
+        this.value = this.domain.get(0);
     }
 
     public int getId() {
@@ -45,15 +57,24 @@ public class Summit {
         this.max = max;
     }
 
+    public List<Integer> getDomain() {
+        return domain;
+    }
+
+    public void setDomain(List<Integer> domain) {
+        this.domain = domain;
+    }
+
+    public void remove() {
+        this.domain.remove(0);
+    }
+
     @Override
     public String toString() {
-        return "Summit{" +
-                "id=" + id +
-                ", value=" + value +
-                ", min=" + min +
-                ", max=" + max +
+        return "Summit" + id + " {" +"value=" + value + "domaine=" + domain +
                 '}';
     }
+
 }
 
 
